f8180f69089070490c3d8bda6ca2ffa5b7ef1e40
unknown <>
2017-02-04 23:45:38 +0900

    test commit

---
+++ SportsStore.WebUI/InfraStructure/Binders/CartModelBinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.InfraStructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //세션에서 Cart 개체 가져오기
+            Cart cart = null;
+            if(controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            //세션 데이터에 Cart 개체가 없다면 새로 생성한다.
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+
+            return cart;
+        }
+    }
+}
---
+++ SportsStore.WebUI/InfraStructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.InfraStructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public Object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
