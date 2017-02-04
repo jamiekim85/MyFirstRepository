e25ff1784a1808ce08932e77cd500e2fcdcee972
jamiekim85 <dhzlwkzl@naver.com>
2017-02-05 00:23:34 +0900

    Update Product.cs

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*-------------------------------------
+주석     
+--------------------------------------*/
 namespace SportsStore.Domain.Entities
 {
     public class Product
